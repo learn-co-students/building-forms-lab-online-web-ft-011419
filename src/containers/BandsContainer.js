5169d013098f74966be126ce692b2ae5eb53508c
gbs4ever <gbs4ever2@gmail.com>
2019-06-07 16:21:26 -0400

    Done.

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
-
+import BandInput from '../components/BandInput'
+import { connect } from 'http2';
+//import QuoteForm from './containers/QuoteForm';
 class BandsContainer extends Component {
   render() {
     return(
       <div>
-        BandsContainer
+        
+        <BandInput addBand={this.props.manageBand}/>
       </div>
     )
   }
 }
 
-export default BandsContainer
+
+const mapDispatchToProps = dispatch => ({ addBand: band => dispatch({ type: 'ADD_BAND', band }) });
+
+export default connect(null, mapDispatchToProps ) (BandsContainer)
