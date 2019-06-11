051e600457e3cc923f7189f3209052603c77706d
gbs4ever <gbs4ever2@gmail.com>
2019-06-11 13:11:04 -0400

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
- const Bands = props => {
-//   const bands = props.bands.map((band, index) => {
-//     return <li key={index}>{band.name}</li>;
-//   });
 
-//   return (
-//     <div>
-      
-//     </div>
-//   );
- };
+
+const Bands = props => {
+  const bands = props.bands.map((band, index) => {
+    return <li key={index}>{band.name}</li>;
+  });
+
+  return (
+    <div>
+      {bands}
+    </div>
+  );
+
+};
 
 export default Bands;
