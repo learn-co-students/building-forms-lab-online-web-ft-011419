3cd23a1627661ddb5f4ce03dfa760d44b04d4cdd
Luis Zabala <luis.zabala076@gmail.com>
2019-06-21 01:02:46 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,12 @@
+// manage band will take one action and concat a new band each time the action is dispatched
+
 export default function manageBand(state = {
   bands: [],
 }, action) {
-  return state
+  switch (action.type) {
+    case 'ADD_BAND':
+      return { ...state, bands: [...state.bands, action.band] }
+    default:
+      return state;
+  }
 };
